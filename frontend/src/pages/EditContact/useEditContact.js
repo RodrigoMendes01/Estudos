bbb8e118dfc8f04f41dc334e5c6044d2859410c7
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-11-27 18:32:08 -0300

    refactoring home, newcontact, editcontact and adding container/presentational

---
+++ frontend/src/pages/EditContact/useEditContact.js
@@ -0,0 +1,58 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+
+export default function useEditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  const history = useHistory();
+  const contactFormRef = useRef(null);
+  const [contactName, setContactName] = useState('');
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        contactFormRef.current.setFieldsValues(contact);
+
+        setIsLoading(false);
+        setContactName(contact.name);
+      } catch (error) {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não foi encontrado!',
+        });
+      }
+    }
+    loadContact();
+  }, [id, history]);
+
+  async function handleSubmit(contact) {
+    try {
+      const updateContact = await ContactsService.updateContact(id, contact);
+
+      setContactName(updateContact.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch (error) {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
+
+  return {
+    isLoading,
+    contactName,
+    contactFormRef,
+    handleSubmit,
+  };
+}
